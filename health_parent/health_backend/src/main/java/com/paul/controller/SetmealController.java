7071ca62705e7f4d550bc92b96ad74c24b92ff7f
Paul <liuhongyiyil@163.com>
2019-11-08 20:19:22 +0800

    bug——redis配置文件

--- health_parent/health_backend/src/main/java/com/paul/controller/SetmealController.java
+++ health_parent/health_backend/src/main/java/com/paul/controller/SetmealController.java
@@ -2,22 +2,29 @@ package com.paul.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.paul.constant.MessageConstant;
+import com.paul.constant.RedisConstant;
 import com.paul.entity.PageResult;
 import com.paul.entity.QueryPageBean;
 import com.paul.entity.Result;
 import com.paul.pojo.Setmeal;
 import com.paul.service.SetmealService;
 import com.paul.utils.QiniuUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import redis.clients.jedis.JedisPool;
+
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/setmeal")
 public class SetmealController {
+
+    @Autowired
+    private JedisPool jedisPool;
 
     @Reference
     private SetmealService setmealService;
@@ -38,6 +45,9 @@ public class SetmealController {
             //拼接文件名
             String fileName = UUID.randomUUID().toString() + suffix;
             QiniuUtils.upload2Qiniu(multipartFile.getBytes(),fileName);
+            //存入大集合
+            jedisPool.getResource().sadd(RedisConstant.SETMEAL_PIC_RESOURCES,fileName);
+
             return new Result(true, MessageConstant.PIC_UPLOAD_SUCCESS,fileName);
         }catch (Exception e){
             e.printStackTrace();
--- health_parent/health_service_provider/src/main/java/com/paul/service/impl/SetmealServiceImpl.java
+++ health_parent/health_service_provider/src/main/java/com/paul/service/impl/SetmealServiceImpl.java
@@ -3,6 +3,7 @@ package com.paul.service.impl;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
+import com.paul.constant.RedisConstant;
 import com.paul.dao.SetmealDao;
 import com.paul.entity.PageResult;
 import com.paul.entity.QueryPageBean;
@@ -10,6 +11,8 @@ import com.paul.pojo.Setmeal;
 import com.paul.service.SetmealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+import redis.clients.jedis.JedisPool;
+
 
 import java.util.HashMap;
 import java.util.List;
@@ -21,7 +24,12 @@ public class SetmealServiceImpl implements SetmealService {
     @Autowired
     private SetmealDao setmealDao;
 
+    @Autowired
+    private JedisPool jedisPool;
+
+
     @Override
+
     public PageResult findPage(QueryPageBean queryPageBean) {
         //获取分页 模糊查询信息
         Integer currentPage = queryPageBean.getCurrentPage();
@@ -29,25 +37,29 @@ public class SetmealServiceImpl implements SetmealService {
         String queryString = queryPageBean.getQueryString();
 
         //分页
-        PageHelper.startPage(currentPage,pageSize);
+        PageHelper.startPage(currentPage, pageSize);
         Page<Setmeal> setmeals = setmealDao.findPage(queryString);
 
         List<Setmeal> list = setmeals.getResult();
         long total = setmeals.getTotal();
 
-        return new PageResult(total,list);
+        return new PageResult(total, list);
     }
 
     @Override
     public void add(Setmeal setmeal, Integer[] checkgroupIds) {
         setmealDao.add(setmeal);
-        if(checkgroupIds !=null && checkgroupIds.length > 0){
+
+        if ( checkgroupIds != null && checkgroupIds.length > 0 ) {
             for (Integer checkgroupId : checkgroupIds) {
                 HashMap<String, Integer> map = new HashMap<>();
-                map.put("checkgroupId",checkgroupId);
-                map.put("setmealId",setmeal.getId());
+                map.put("checkgroupId", checkgroupId);
+                map.put("setmealId", setmeal.getId());
                 setmealDao.setSetmealAndCheckgroup(map);
             }
         }
+
+        //jedisPool.getResource().sadd(RedisConstant.SETMEAL_PIC_DB_RESOURCES,setmeal.getImg());
+
     }
 }
