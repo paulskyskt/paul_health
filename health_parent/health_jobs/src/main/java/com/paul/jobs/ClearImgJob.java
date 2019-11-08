0994d3565bc34a08a0318cc48d4264698b2f88d8
Paul <liuhongyiyil@163.com>
2019-11-08 21:32:02 +0800

    清除垃圾图片完成

---
+++ health_parent/health_jobs/src/main/java/com/paul/jobs/ClearImgJob.java
@@ -0,0 +1,27 @@
+package com.paul.jobs;
+
+import com.paul.constant.RedisConstant;
+import com.paul.utils.QiniuUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import redis.clients.jedis.JedisPool;
+
+import java.util.Set;
+
+public class ClearImgJob {
+    @Autowired
+    private JedisPool jedisPool;
+
+    public void clearImg(){
+
+        System.out.println("clear");
+
+        Set<String> set = jedisPool.getResource().sdiff(RedisConstant.SETMEAL_PIC_RESOURCES, RedisConstant.SETMEAL_PIC_DB_RESOURCES);
+
+        if(set!=null){
+            for (String name : set) {
+                QiniuUtils.deleteFileFromQiniu(name);
+                jedisPool.getResource().srem(RedisConstant.SETMEAL_PIC_RESOURCES,name);
+            }
+        }
+    }
+}
